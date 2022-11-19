d1087307d2b7292a6b43d47b309226b8d10d5bb6
Steve Vezeau <svezer@live.ca>
2022-11-19 13:03:33 -0800

    add node-fetch

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-export default () => (
-  <div className="layout">
-    <div className="site-layout-content" style={{ margin: "100px auto" }}>
-      <h1>Title</h1>
+export default () => {
+  const [s, setS] = useState();
+
+  useEffect(() => {});
+
+  return (
+    <div className="layout">
+      <div className="site-layout-content" style={{ margin: "100px auto" }}>
+        <h1>Title</h1>
+      </div>
+      <footer style={{ textAlign: "center" }}>foot..</footer>
     </div>
-    <footer style={{ textAlign: "center" }}>foot..</footer>
-  </div>
-);
+  );
+};
