7eb72a111d180e6fdb617a954a47361a9ef84148
Steve Vezeau <svezer@live.ca>
2022-11-19 13:01:06 -0800

    move all files to root

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () => (
+  <div className="layout">
+    <div className="site-layout-content" style={{ margin: "100px auto" }}>
+      <h1>Title</h1>
+    </div>
+    <footer style={{ textAlign: "center" }}>foot..</footer>
+  </div>
+);
